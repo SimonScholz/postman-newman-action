536a689b99abf278e4f0bb81a22d13ab185d6c53
Simon Scholz <scholz@doorbit.com>
2023-09-16 00:11:38 +0200

    Regnerate dist folder

--- src/output.ts
+++ src/output.ts
@@ -1,18 +1,20 @@
 import * as newman from 'newman'
 
-export function createResultOutputFromSummary(summary: newman.NewmanRunSummary) : string {
-    const failures = summary.run.failures.map(failure => {
-        return {
-            name: failure.source?.name,
-            test: failure.error.name,
-            message: failure.error.message
-        }
-    })
+export function createResultOutputFromSummary(
+  summary: newman.NewmanRunSummary
+): string {
+  const failures = summary.run.failures.map(failure => {
+    return {
+      name: failure.source?.name,
+      test: failure.error.name,
+      message: failure.error.message
+    }
+  })
 
-    return JSON.stringify({
-        collection: summary.collection.name,
-        failures: failures,
-        Requests: summary.run.stats.requests,
-        Assertions: summary.run.stats.assertions
-    })
+  return JSON.stringify({
+    collection: summary.collection.name,
+    failures,
+    Requests: summary.run.stats.requests,
+    Assertions: summary.run.stats.assertions
+  })
 }
