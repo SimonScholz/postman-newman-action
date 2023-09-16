7695f230a84e4243510a8668924b88e919f84a38
Simon Scholz <scholz@doorbit.com>
2023-09-16 13:20:08 +0200

    Documented inputs and outputs for the action

--- src/output.ts
+++ src/output.ts
@@ -1,6 +1,29 @@
 import * as newman from 'newman'
+import * as core from '@actions/core'
 
-export function createResultOutputFromSummary(
+export function handleOutputs(summary: newman.NewmanRunSummary) {
+  outputSummary(summary)
+  outputOriginalSummary(summary)
+}
+
+function outputSummary(summary: newman.NewmanRunSummary) {
+  core.setOutput('summary', createResultOutputFromSummary(summary))
+}
+
+function outputOriginalSummary(summary: newman.NewmanRunSummary) {
+  if(core.getBooleanInput('outputOriginalSummary')) {
+    core.setOutput('originalSummary', JSON.stringify(summary))
+  }
+}
+
+function outputGoogleCardV2(summary: newman.NewmanRunSummary) {
+  if(core.getBooleanInput('outputGoogleCardV2')) {
+    // TODO Create google card v2 structure
+    // core.setOutput('googleCardV2', JSON.stringify(summary))
+  }
+}
+
+function createResultOutputFromSummary(
   summary: newman.NewmanRunSummary
 ): string {
   const failures = summary.run.failures.map(failure => {
