f6daa83813f9b9dee2f58fa96f17733a21f600a4
Simon Scholz <scholz@doorbit.com>
2023-09-20 21:07:33 +0200

    Make use of br instead of slash n

--- src/output.ts
+++ src/output.ts
@@ -90,7 +90,7 @@ function createGoogleCardV2StructureOutput(
             'https://cdn2.iconfinder.com/data/icons/kids/128x128/apps/agt_action_fail.png'
         },
         wrapText: false,
-        text: `<b>Name:</b> ${failure.source?.name} \n<b>Test:</b> ${failure.error.test} \n<b>Message:</b> ${failure.error.message}`
+        text: `<b>Name:</b> ${failure.source?.name} <br><b>Test:</b> ${failure.error.test} <br><b>Message:</b> ${failure.error.message}`
       }
     }
   })
