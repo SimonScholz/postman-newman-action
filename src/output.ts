402ae874806722c2d754531b42d141f21ebb456d
Simon Scholz <scholz@doorbit.com>
2023-10-16 14:07:50 +0200

    Escape message of failure

--- src/output.ts
+++ src/output.ts
@@ -84,14 +84,20 @@ function createGoogleCardV2StructureOutput(
             'https://raw.githubusercontent.com/SimonScholz/google-chat-action/main/assets/failure-128.png'
         },
         wrapText: true,
-        text: `<b>Name:</b> ${makeJQAndBashFriendly(failure.source?.name)} <br><b>Test:</b> ${makeJQAndBashFriendly(failure.error.test)} <br><b>Message:</b> ${makeJQAndBashFriendly(failure.error.message)}`
+        text: `<b>Name:</b> ${makeJQAndBashFriendly(
+          failure.source?.name
+        )} <br><b>Test:</b> ${makeJQAndBashFriendly(
+          failure.error.test
+        )} <br><b>Message:</b> ${makeJQAndBashFriendly(failure.error.message)}`
       }
     }
   })
 
   const headerText =
     summary.run.failures.length > 0
-      ? `<font color="#FF0B0B">${makeJQAndBashFriendly(summary.collection.name)} - ${summary.run.failures.length} Failure(s)</font>`
+      ? `<font color="#FF0B0B">${makeJQAndBashFriendly(
+          summary.collection.name
+        )} - ${summary.run.failures.length} Failure(s)</font>`
       : makeJQAndBashFriendly(summary.collection.name)
 
   return [
@@ -111,6 +117,8 @@ function createGoogleCardV2StructureOutput(
  * @param jsonString json string that may contain single quotes
  * @returns the JSON string without single quotes
  */
-function makeJQAndBashFriendly(jsonString: string | undefined): string | undefined {
-  return jsonString?.replace(/'/g, '').replace( /(<([^>]+)>)/ig, '')
+function makeJQAndBashFriendly(
+  jsonString: string | undefined
+): string | undefined {
+  return jsonString?.replace(/'/g, '').replace(/(<([^>]+)>)/gi, '')
 }
